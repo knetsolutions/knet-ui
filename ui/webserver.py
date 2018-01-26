cf1dbca8509012f6f6de945958e90ffb29f6016e
suresh <sureshkumarr.s@gmail.com>
2018-01-26 20:43:02 +0530

    fixed the data.js issue

--- ui/webserver.py
+++ ui/webserver.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template
 from flask import jsonify
 import sys
+import os
+import subprocess
 
 fname = "index.html"
 
@@ -10,7 +12,24 @@ app = Flask(__name__)
 def static_file(path):
     return app.send_static_file(path)
 
+
+
+# Executing Command
+def run_cmd(cmd):
+    try:
+        print(str(cmd))
+        return subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as ex:
+        print "return code", ex.returncode
+        print "cmd execution returned exit status", ex.output.strip()
+
 def main():
+    # create a soft link /tmp/data.js to static/data.js
+    cpath = os.path.dirname(os.path.abspath(__file__)) + "/static/data.js"
+    cmd = ["rm", cpath]
+    run_cmd(cmd)
+    cmd = ["ln", '-s', '/tmp/data.js', cpath]
+    run_cmd(cmd)
     app.run(host="0.0.0.0", port=5000, debug=True)
 
 
